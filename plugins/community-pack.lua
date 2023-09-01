8acef50492f7614c19c9a9ffcfa4a9c1e1b26b94
Sinclair Lin <lzx2155433@gmail.com>
2023-09-01 20:12:43 +0800

    feat: add python development environment

---
+++ plugins/community-pack.lua
@@ -0,0 +1,6 @@
+return {
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.python" },
+}
--- plugins/community.lua
+++ plugins/community.lua
@@ -6,7 +6,4 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.html-css" }
 }
