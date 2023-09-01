2824a8e1dec300ff18a07998098c4fdb6a36e56c
Sinclair Lin <lzx2155433@gmail.com>
2023-09-01 19:33:50 +0800

    add lsp server

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.html-css" }
 }
